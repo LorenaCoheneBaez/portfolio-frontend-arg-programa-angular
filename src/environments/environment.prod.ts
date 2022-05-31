47f2b5e9faf6a13e8dbb836f3275cbb4956977c3
LorenaCoheneBaez <loreley.cb15@gmail.com>
2022-05-31 14:49:49 -0300

    Corrijo auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://portfolio-cohene-arg-programa.herokuapp.com/',
-  apiAuthUrl: 'https://portfolio-cohene-arg-programa.herokuapp.com/auth/',
+  apiAuthUrl: 'https://portfolio-cohene-arg-programa.herokuapp.com/',
 };
